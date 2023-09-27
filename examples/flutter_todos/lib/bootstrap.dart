3f1109b1fa6960147fc62c28508b2baee0ad7577
Emirhan Hasanoglu <emirhanhasanoglu@proton.me>
2023-09-27 11:06:21 +0300

    feat(main): Collections subscription to bootstrap

--- examples/flutter_todos/lib/bootstrap.dart
+++ examples/flutter_todos/lib/bootstrap.dart
@@ -7,8 +7,10 @@ import 'package:flutter_todos/app/app.dart';
 import 'package:flutter_todos/app/app_bloc_observer.dart';
 import 'package:todos_api/todos_api.dart';
 import 'package:todos_repository/todos_repository.dart';
+import 'package:collections_api/collections_api.dart';
+import 'package:collections_repository/collections_repository.dart';
 
-void bootstrap({required TodosApi todosApi}) {
+void bootstrap({required TodosApi todosApi, required CollectionsApi collectionsApi}) {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
@@ -16,9 +18,10 @@ void bootstrap({required TodosApi todosApi}) {
   Bloc.observer = const AppBlocObserver();
 
   final todosRepository = TodosRepository(todosApi: todosApi);
+  final collectionsRepository = CollectionsRepository(collectionsApi: collectionsApi);
 
   runZonedGuarded(
-    () => runApp(App(todosRepository: todosRepository)),
+    () => runApp(App(todosRepository: todosRepository, collectionsRepository: collectionsRepository,)),
     (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
   );
 }
--- examples/flutter_todos/lib/main_development.dart
+++ examples/flutter_todos/lib/main_development.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_todos/bootstrap.dart';
+import 'package:local_storage_collections_api/local_storage_collections_api.dart';
 import 'package:local_storage_todos_api/local_storage_todos_api.dart';
 
 Future<void> main() async {
@@ -9,5 +10,9 @@ Future<void> main() async {
     plugin: await SharedPreferences.getInstance(),
   );
 
-  bootstrap(todosApi: todosApi);
+  final collectionsApi = LocalStorageCollectionsApi(
+    plugin: await SharedPreferences.getInstance(),
+  );
+
+  bootstrap(todosApi: todosApi, collectionsApi: collectionsApi);
 }
